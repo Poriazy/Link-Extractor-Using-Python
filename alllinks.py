3d446e3b4c5ea4c3d06fdd819c07cc7ce6532e18
Poria Zed <poriazeyd@gmail.com>
2019-04-27 16:33:03 +0430

    add user format filter

--- alllinks.py
+++ alllinks.py
@@ -4,7 +4,7 @@ import urllib.request #importing the request library
 Creating a function to read the url using request library.
 Also decoding it to be easily useable.
 """
-def get_page(url):
+def from_page(url):
     try:
         with urllib.request.urlopen(url) as response:
             html = response.read().decode()
@@ -15,7 +15,6 @@ def get_page(url):
 """
 Creating a function to extract the links from the recieved data.
 """
-
 def get_next_target(s):
     start_link=s.find('<a href="htt')
     if start_link == -1:
@@ -24,17 +23,37 @@ def get_next_target(s):
     end_quote=s.find('"',start_quote+1)
     url=s[start_quote+1:end_quote]
     return url,end_quote
+
 """
 Creating a function to print the extracted links from the requested web page.
 """
-def print_all_links(page):
+def retrive_all_links(page):
+    urls = []
     while True:
-        url,endpos = get_next_target(page)
+        url,endpos = get_next_target(page)        
         if url:
-            print (url)
-            page=page[endpos:]
+            # print (url)
+            urls.append(url)
+            page = page[endpos:]
         else:
             break
+    return urls
 
-print_all_links(get_page('https://en.wikipedia.org/wiki/Abraham_Lincoln'))
+web_page_address = input ("please enter your target web page: ")
+file_format = input("now enter file format or string that you need (-a for all links): ")
 
+urls_list = retrive_all_links(from_page(web_page_address))
+all_link_size = len(urls_list)
+
+custom_format_link_size = 0
+
+for url in urls_list:
+    if file_format in url:
+        print (url)
+        custom_format_link_size += 1
+    elif file_format == "-a":
+        custom_format_link_size = all_link_size
+        print (url)
+    
+print ("match format size:", custom_format_link_size)
+print ("links in webpage:", all_link_size)
